d9a6513863204e54413b601603c993bb5a78e079
mocavada <marc@awesomewebsite.ca>
2022-01-24 09:15:55 -0500

    first commit

---
+++ trainingproject/_Resource/UsingJCRMavenTests/Using Java Content Repository Maven Tests/TestableClass.java
@@ -0,0 +1,31 @@
+package com.adobe.training.core;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+public class TestableClass {
+	protected static final String CONTENT_NODENAME = "content";
+	
+	public String stripNonLettersOrNumbers(String in) {
+		if(in != null) {
+			return in.replaceAll( "[^\\p{L}\\p{N}]", "");
+		} else {
+			return null;
+		}
+		
+	}
+
+	
+	public String getContentPath(Session session)  throws RepositoryException {
+		Node rootNode = session.getRootNode();
+		if (rootNode.hasNode(CONTENT_NODENAME))  {
+			Node contentNode = rootNode.getNode(CONTENT_NODENAME);
+			return contentNode.getPath();
+		
+		} else {
+			return rootNode.getPath();
+		}
+	}
+	
+}
